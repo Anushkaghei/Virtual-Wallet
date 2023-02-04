f74f60e331639e1cdde2958486e9a91d80920110
Anushka Ghei <79694271+Anushkaghei@users.noreply.github.com>
2023-02-04 22:31:21 +0530

    Add files via upload

---
+++ Customer_Billing.h
@@ -0,0 +1,15 @@
+
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+
+ #include<stdio.h>
+ #include <string.h>
+ #include<stdlib.h>
+
+ int createAccount();//for new customers
+ void searchACustomer(int act_no);//searches for a particular customer id
+ void payBill(int act_no);//pays bill from the e-wallet
+ void printAllCustomers();//prints info for all customers
+ void addBalance(int act_no);//add balance amount to e wallet
+
+#endif
